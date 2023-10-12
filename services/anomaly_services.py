e52a50b5d8aac9659bea84afa7869a308fb18b98
Alan B <death1027@outlook.com>
2023-10-12 00:29:52 -0600

    feat: :construction: add csv,xlsx and array methods to extract information in anomaly services

--- services/anomaly_services.py
+++ services/anomaly_services.py
@@ -1,34 +1,50 @@
 import pandas as pd
 import numpy as np
+from typing import Union
 
 
 class DetectAnomaly:
-    def __init__(self, data, window_size=10, threshold=2):
-        self.data = pd.DataFrame(data)
-        self.data.plot()
+    def __init__(
+        self, data: Union[list, str, bytes], window_size=10, threshold=2, main_column=0
+    ):
+        self.data = data
         self.window_size = window_size
         self.threshold = threshold
+        self.main_column = main_column
 
-    def detect(self) -> list:
+    def __detect(self) -> list:
         """
         Detect anomalies in the data using rolling mean and standard deviation
         """
         window_size = self.window_size
         threshold = self.threshold
         data = self.data
-        data["bottom"] = data[0].rolling(window=window_size).mean() - (
-            threshold * data[0].rolling(window=window_size).std()
+        main_column = self.main_column
+        data["bottom"] = data[main_column].rolling(window=window_size).mean() - (
+            threshold * data[main_column].rolling(window=window_size).std()
         )
-        data["top"] = data[0].rolling(window=window_size).mean() + (
-            threshold * data[0].rolling(window=window_size).std()
+        data["top"] = data[main_column].rolling(window=window_size).mean() + (
+            threshold * data[main_column].rolling(window=window_size).std()
         )
         data.plot()
         data["anomaly"] = data.apply(
-            lambda row: row[0]
-            if (row[0] <= row["bottom"] or row[0] >= row["top"])
+            lambda row: row[main_column]
+            if (row[main_column] <= row["bottom"] or row[main_column] >= row["top"])
             else np.nan,
             axis=1,
         )
         data.plot()
         self.data = data
         return data["anomaly"].dropna()
+
+    def csv(self) -> list:
+        self.data = pd.read_csv(self.data)
+        return self.__detect()
+
+    def array(self) -> list:
+        self.data = pd.DataFrame(self.data)
+        return self.__detect()
+
+    def xlsx(self) -> list:
+        self.data = pd.read_excel(self.data)
+        return self.__detect()
--- services/tests/anomaly_test.py
+++ services/tests/anomaly_test.py
@@ -7,10 +7,50 @@ sys.path.append("../..")
 from services.anomaly_services import DetectAnomaly
 
 
+def create_csv():
+    data = np.random.randn(250)
+    df = pd.DataFrame(data, columns=["data"])
+    df.to_csv("./test.csv")
+
+
+def delete_csv():
+    import os
+
+    os.remove("./test.csv")
+
+
+def create_xlsx():
+    data = np.random.randn(250)
+    df = pd.DataFrame(data, columns=["data"])
+    df.to_excel("./test.xlsx")
+
+
+def delete_xlsx():
+    import os
+
+    os.remove("./test.xlsx")
+
+
 class AnalyticTesting(TestCase):
-    def test_create_process(self):
+    def test_create_process_array(self):
         data = np.random.randn(250)
         anomaly_service = DetectAnomaly(data, 20)
-        anomaly = anomaly_service.detect()
+        anomaly = anomaly_service.array()
+        assert len(anomaly) > 0
+        assert type(anomaly) == pd.Series
+
+    def test_create_process_csv(self):
+        create_csv()
+        anomaly_service = DetectAnomaly("./test.csv", 20, main_column="data")
+        anomaly = anomaly_service.csv()
+        delete_csv()
+        assert len(anomaly) > 0
+        assert type(anomaly) == pd.Series
+
+    def test_create_process_xlsx(self):
+        create_xlsx()
+        anomaly_service = DetectAnomaly("./test.xlsx", 20, main_column="data")
+        anomaly = anomaly_service.xlsx()
+        delete_xlsx()
         assert len(anomaly) > 0
         assert type(anomaly) == pd.Series
